4aa4f88339ba529302d7f53e57777cb8d61bbf95
Shreyas Mahajan <shreyasmahajan05@gmail.com>
2024-09-21 22:20:39 +0530

    allow host

--- face_rec/settings.py
+++ face_rec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%nl)!bf_9seqkg+ejk$o%qztc9fpl-s%j&%q=0bigth%)(^j1!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['4.213.155.9', 'localhost', '127.0.0.1']
 
 
 # Application definition
